680b300e06cfcacdea67ac147f697f92735f42ae
alstn1234 <rhalstn8549@gmail.com>
2023-12-13 18:09:03 +0900

    Fix: UI
    - EquipUI, UnEquipUI 팝업을 띄우면 다른 버튼이 클릭되지 않도록 수정

--- SpartaDungeon/Assets/Scripts/UI/InventoryUI.cs
+++ SpartaDungeon/Assets/Scripts/UI/InventoryUI.cs
@@ -10,7 +10,6 @@ public class InventoryUI : MonoBehaviour
 
     public GameObject equipUI;
     public GameObject unEquipUI;
-    public Button backButton;
 
     private void Awake()
     {
@@ -26,26 +25,22 @@ public class InventoryUI : MonoBehaviour
             equipUI.SetActive(true);
         else
             unEquipUI.SetActive(true);
-        backButton.interactable = false;
     }
 
     public void CancleClick()
     {
-        backButton.interactable = true;
         if (equipUI.activeSelf) equipUI.SetActive(false);
         if (unEquipUI.activeSelf) unEquipUI.SetActive(false);
     }
 
     public void EquipConfirmClick()
     {
-        backButton.interactable = true;
         equipUI.SetActive(false);
         _controller.CallEquipEvent(_item);
     }
 
     public void UnEquipConfirmClick()
     {
-        backButton.interactable = true;
         unEquipUI.SetActive(false);
         _controller.CallUnEquipEvent(_item);
     }
